fe2f53166f43c5b90952455edf0615ab56395cbd
Kimgyoomin <kimgyoomin1999@gmail.com>
2024-02-16 15:12:16 +0900

    while문 사용하고 파일 입출력문 수정 완료 24-02-16

--- z1_sdk/examples/example_joint_ctrl.cpp
+++ z1_sdk/examples/example_joint_ctrl.cpp
@@ -5,57 +5,17 @@
 #include <chrono>
 #include <fstream>
 #include <iomanip>
-#include <pthread.h>
+#include <vector>
 
+/**
+ * @example example_joint_ctrl.cpp
+ * An example showing how to control joint velocity and position.
+ * 
+ * Run `roslaunch z1_bringup **_ctrl.launch` first.
+ */
 
 /* 그래프 그리기 위해 데이터 뽑기 */
-// using std::ofstream;
-
-using namespace UNITREE_ARM_SDK;
-
-
-
-// 관절 위치 제어를 수행하는 thread func
-void* controlLoop(void* arg) {
-  UnitreeArm* z1 = static_cast<UnitreeArm*>(arg); // 스레드 인자로 받은 로봇 객체 포인터
-
-  // 로봇의 초기 상태 수신
-  z1->recv();
-
-
-  double jntSpeed = 5 * M_PI / 180; // 관절 속도 (rad/s)
-  Vec6 q; // 관절 위치를 저장할 벡터
-  int cnt = 0; // 카운터 초기화
-
-  while(1) {
-    if (cnt >= 10250) break; // 반복 횟수 제한
-    z1->armCmd.mode = static_cast<mode_t>(ArmMode::JointPositionCtrl);
-    z1->recv();
-
-    q = z1->armState.getQ_d();
-
-    // JT5, JT4 위치 변화
-    q(5) = M_PI / 8 * (1 - cos(jntSpeed * cnt * 0.00328));
-    z1->armCmd.dq_d[5] = M_PI / 8 * sin(jntSpeed * cnt * 0.00328);
-    
-    // q(4) = M_PI / 8 * (1 - cos(jntSpeed * cnt * 0.00328));
-    // z1->armCmd.dq_d[4] = M_PI / 8 * sin(jntSpeed * cnt * 0.00328);
-
-    z1->send();
-    z1->recv();
-
-    cnt++;
-  }
-
-  // 스레드 종료 시 로봇을 Passive 모드로 설정
-  z1->armCmd.mode = static_cast<mode_t>(ArmMode::Passive);
-  z1->sendRecv();
-
-  return NULL;
-}
-
-
-
+using std::ofstream;
 
 int main(int argc, char** argv)
 {
@@ -64,65 +24,64 @@ int main(int argc, char** argv)
   UNITREE_ARM_SDK::UnitreeArm z1(controller_IP);
   z1.init();
 
-  // ofstream file("/home/kim/rc_lab_datafolder/z1_jt5jt4_test1.txt");
-  z1.recv();
-  Timer timer(z1.dt);
+  ofstream file("/home/kim/rc_lab_datafolder/240215_1.txt");
 
-  pthread_t threadID;
-  pthread_create(&threadID, NULL, controlLoop, &z1);
-  pthread_join(threadID, NULL);
+  // Timer timer(z1.dt);
+  double jntSpeed = 45 * M_PI / 180;
+  std::vector<float> save_ref_5;
+  std::vector<float> save_enc_5;
+  std::vector<float> save_ref_4;
+  std::vector<float> save_enc_4;
 
-
-
-
-  /* Joint velocity control */
-  // double jntSpeed = 5 * M_PI / 180;
-
-  /* chrono func start*/
-  // auto start = std::chrono::high_resolution_clock::now();
 
   /* Joint position control */
   // Get current desired q
-  // Vec6 q = z1.armState.getQ_d();
-  // for (size_t i = 0; i < 10250; i++)
-  // {
-  //   z1.armCmd.mode = (mode_t)UNITREE_ARM_SDK::ArmMode::JointPositionCtrl;
-  //   q(5) = M_PI / 8 * (1 - cos(jntSpeed * 2 * 0.00328 * i));
-  //   z1.armCmd.setQ(q);
-  //   z1.send();
-  //   z1.armCmd.dq_d[5] = M_PI / 8 * sin(jntSpeed * 2 * 0.00328);
-  //   q(4) = M_PI / 8 * (1 - cos(jntSpeed * 2 * 0.00328 * i));
-  //   z1.armCmd.setQ(q);
-  //   z1.armCmd.dq_d[4] = M_PI / 8 * sin(jntSpeed * 2 * 0.00328);
-  //   // z1.armCmd.gripperCmd.angle -= jntSpeed*z1.dt;
-  //   z1.send();
-  //   z1.recv();
-  //   // timer.sleep();
+  Vec6 q = z1.armState.getQ_d();
 
-  //   file << std::fixed << std::setprecision(6) << q[5] << "\t"
-  //   << z1.armState.q[5] << "\t" << q[4] << "\t" << z1.armState.q[4]
-  //   << std::endl;
-  // }
+  int cnt = 0;
+  auto start = std::chrono::high_resolution_clock::now();
+  while(1) {
+    z1.armCmd.mode = (mode_t)UNITREE_ARM_SDK::ArmMode::JointPositionCtrl;
+    z1.recv();
+    if (cnt >= 1250) break;
 
-  /* chrono func finish */
-  // auto end = std::chrono::high_resolution_clock::now();
-  // std::chrono::duration<double, std::milli> execution_time = end - start;
-  // std::cout << "Execution Time (chorno) " << execution_time.count() << "ms"
-  // << std::endl;
-  // Vec6 q = z1.armState.getQ_d();
-  // for (size_t i = 0; i < 1250; i ++)
-  // {
-  //   z1.armCmd.mode = (mode_t)UNITREE_ARM_SDK::ArmMode::JointPositionCtrl;
-  //   q(4) = M_PI / 8 * (1 - cos(jntSpeed * 2 * 0.00328 * i));
-  //   z1.armCmd.setQ(q);
-  //   z1.armCmd.dq_d[4] = M_PI / 8 * sin(jntSpeed * 2 * 0.00328);
-  //   // z1.armCmd.gripperCmd.angle -= jntSpeed*z1.dt;
-  //   z1.send();
-  //   z1.recv();
-  //   // timer.sleep();
-  // }
-  // z1.armCmd.mode = (mode_t)UNITREE_ARM_SDK::ArmMode::Passive;
-  // z1.sendRecv();
+    // 관절 제어부
+    z1.armCmd.setQ(q);
+    q(5) = M_PI / 8 * (1 - cos(jntSpeed * 0.0064 * cnt));
+    z1.armCmd.dq_d[5] = M_PI / 8 * sin(jntSpeed * 0.0064);
+
+    q(4) = M_PI / 8 * (1 - cos(jntSpeed * 0.0064 * cnt));
+    z1.armCmd.dq_d[4] = M_PI / 8 * sin(jntSpeed * 0.0064);
+
+    z1.send();
+    save_ref_5.push_back(q(5));
+    save_enc_5.push_back(z1.armState.q[5]);
+    save_ref_4.push_back(q(4));
+    save_enc_4.push_back(z1.armState.q[4]);
+
+    // 고해상도 타이머를 통해 정확하게 한 iteration당 4ms 되도록 대기
+    auto next_cycle = start + std::chrono::milliseconds(4 * (cnt+1));
+    std::this_thread::sleep_until(next_cycle);
+    cnt++;
+  }
+
+  /* chrono func finish 
+  checking for time */
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double, std::milli> execution_time = end - start;
+  std::cout << "Execution Time (chorno) " << execution_time.count() << "ms"
+  << std::endl;
+
+  /* 작업 끝난 후 arm모드를 Passive로 변경 */
+  z1.armCmd.mode = (mode_t)UNITREE_ARM_SDK::ArmMode::Passive;
+  z1.sendRecv();
+
+  for (size_t i = 0; i < 1250; i++)
+  {
+    file << std::fixed << std::setprecision(6) << save_ref_5[i]
+    << "\t" << save_enc_5[i] << "\t" << save_ref_4[i] << "\t" <<
+    save_enc_4[i] << std::endl;
+  }
 
   /* end for writing file */
   // file.close();
